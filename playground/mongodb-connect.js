a07f40691b36b23da739b162a282efce9172282e
kikmaster <pranout_kik@hotmail.com>
2019-01-13 15:40:08 +0700

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c3af30bbe9c3407f4b5664d')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // })
+
+    // db.collection('Todos').find({}).count().then((count) => {
+    //     console.log('Todos count: ', count);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // })
+
+    db.collection('Users').find({name:'Kik'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+        var id = new ObjectID(docs[1]._id);
+        console.log(id.getTimestamp());
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+
+
+    client.close();
+
+});
