0728528cf6daa204f697789393aac222aacefb14
kikmaster <pranout_kik@hotmail.com>
2019-01-18 20:50:40 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,27 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todo/1234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    //Valid id using isValid
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    // findById
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
